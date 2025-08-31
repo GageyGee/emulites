bf18e3d5b729db774387aaf285d2f2c62c225be7
GageyGee <ggqweasdzxc15@gmail.com>
2025-08-31 19:56:19 -0400

    Create solana-config.js

---
+++ public/private/solana-config.js
@@ -0,0 +1,6 @@
+// Solana configuration
+export const SOLANA_CONFIG = {
+    RPC_URL: 'https://radial-chaotic-pool.solana-mainnet.quiknode.pro/192e8e76f0a288f5a32ace0b676f7f34778f219f/',
+    THRONG_TOKEN_MINT: 'B9iPvm8YybydhvMiKAuJuygEKuzspgxdavhFNzzUpump',
+    BURN_ADDRESS: '1nc1nerator11111111111111111111111111111111'
+};
